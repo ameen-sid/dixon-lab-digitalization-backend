fada0bd1a37fc0b48689f8e88334da3a24ac6f4e
Ameen Sid <ameensid7@outlook.com>
2026-05-18 13:18:18 +0530

    Added database_url and node_env variables in the config file

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 type ServerConfig = {
 	PORT: number;
+	DATABASE_URL: string;
+	NODE_ENV: string;
 };
 
 const loadEnv = () => {
@@ -12,4 +14,6 @@ loadEnv();
 
 export const serverConfig: ServerConfig = {
 	PORT: Number(process.env.PORT) || 3001,
+	DATABASE_URL: process.env.DATABASE_URL || 'sqlserver://31.97.229.146:1433;database=dixon;user=dixon_user;password=Dixon@12345Sarvagya;encrypt=true;trustServerCertificate=true;',
+	NODE_ENV: process.env.NODE_ENV || 'development'
 };

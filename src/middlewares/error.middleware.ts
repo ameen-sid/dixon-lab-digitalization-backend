87ef08133580007be3dc63bd821771227cb1e68e
Ameen Sid <ameensid7@outlook.com>
2026-05-18 15:28:49 +0530

    Fixed issue of appErrorHandler middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -3,10 +3,13 @@ import { AppError } from '../utils/errors/app.error';
 
 export const appErrorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
 	console.log(err);
-	res.status(err.statusCode).json({
-		success: false,
-		message: err.message,
-	});
+	if (err.statusCode) {
+		return res.status(err.statusCode).json({
+			success: false,
+			message: err.message,
+		});
+	}
+	next(err);
 };
 
 export const genericErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {

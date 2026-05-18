fa4a3e5e731687d145efb70628bc33b2ea7ef45d
Ameen Sid <ameensid7@outlook.com>
2026-05-18 15:28:08 +0530

    Added department router in the v1 router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import pingRouter from './ping.router';
+import departmentRouter from './department.router';
 
 const v1Router = express.Router();
 
 v1Router.use('/ping', pingRouter);
+v1Router.use('/departments', departmentRouter);
 
 export default v1Router;

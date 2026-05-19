f2914df90aac7beaadb7bb656d19c91754028000
Ameen Sid <ameensid7@outlook.com>
2026-05-19 11:38:39 +0530

    Added auth router in the v1 router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import pingRouter from './ping.router';
 import departmentRouter from './department.router';
 import userRouter from './user.router';
+import authRouter from './auth.router';
 
 const v1Router = express.Router();
 
 v1Router.use('/ping', pingRouter);
 v1Router.use('/departments', departmentRouter);
 v1Router.use('/users', userRouter);
+v1Router.use('/auth', authRouter);
 
 export default v1Router;

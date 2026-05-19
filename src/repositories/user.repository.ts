07b648ad55410488fb393694af923772a355c1d4
Ameen Sid <ameensid7@outlook.com>
2026-05-19 10:55:43 +0530

    Added user repository layer

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,43 @@
+import { prisma } from '../configs/prisma.config';
+import { User } from '@prisma/client';
+
+export interface IUserRepository {
+	createUser(user: Omit<User, 'id' | 'createdAt' | 'updatedAt'>): Promise<User>;
+	getUsers(where: any, sortBy: string, sortOrder: string, skip: number, limit: number): Promise<Omit<User, 'password' | 'updatedAt'>[]>;
+	updateUser(id: number, updateData: Omit<User, 'id' | 'createdAt' | 'updatedAt'>): Promise<User | null>;	
+	deleteUser(id: number): Promise<Boolean>;
+}
+
+export class UserRepository implements IUserRepository {
+	async createUser(user: Omit<User, 'id' | 'createdAt' | 'updatedAt'>): Promise<User> {
+		return await prisma.user.create({ data: user });
+	}
+
+	async getUsers(where: any, sortBy: string, sortOrder: string, skip: number, limit: number): Promise<Omit<User, 'password' | 'updatedAt'>[]> {
+		return await prisma.user.findMany({
+			where,
+			select: {
+				id: true,
+				name: true,
+				username: true,
+				role: true,
+				createdAt: true,
+				departmentId: true
+			},
+			orderBy: { [sortBy]: sortOrder },
+			skip,
+			take: limit
+		});
+	}
+
+	async updateUser(id: number, updateData: Omit<User, 'id' | 'createdAt' | 'updatedAt'>): Promise<User | null> {
+		return await prisma.user.update({
+			where: { id },
+			data: updateData
+		});
+	}
+
+	async deleteUser(id: number): Promise<Boolean> {
+		return await prisma.user.delete({ where: { id } }) ? true : false;
+	}
+}

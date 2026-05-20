bb385ab09314835026ce8e22eed233a3e0766048
Ameen Sid <ameensid7@outlook.com>
2026-05-20 15:34:18 +0530

    Added authenticateToken and requireRole middlewares in the v1 router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -4,13 +4,14 @@ import departmentRouter from './department.router';
 import userRouter from './user.router';
 import authRouter from './auth.router';
 import testTypeRouter from './test-type.router';
+import { authenticateToken, requireRole } from '../../middlewares/auth.middleware';
 
 const v1Router = express.Router();
 
 v1Router.use('/ping', pingRouter);
-v1Router.use('/departments', departmentRouter);
-v1Router.use('/users', userRouter);
+v1Router.use('/departments', authenticateToken, requireRole(['Admin']), departmentRouter);
+v1Router.use('/users', authenticateToken, requireRole(['Admin']), userRouter);
 v1Router.use('/auth', authRouter);
-v1Router.use('/test-types', testTypeRouter);
+v1Router.use('/test-types', authenticateToken, requireRole(['Admin']), testTypeRouter);
 
 export default v1Router;

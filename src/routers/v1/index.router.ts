f17897ad0558eb02711f2d2ea4798a8119fbbb06
Ameen Sid <ameensid7@outlook.com>
2026-05-20 14:51:51 +0530

    Added test type router in the v1 router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -3,6 +3,7 @@ import pingRouter from './ping.router';
 import departmentRouter from './department.router';
 import userRouter from './user.router';
 import authRouter from './auth.router';
+import testTypeRouter from './test-type.router';
 
 const v1Router = express.Router();
 
@@ -10,5 +11,6 @@ v1Router.use('/ping', pingRouter);
 v1Router.use('/departments', departmentRouter);
 v1Router.use('/users', userRouter);
 v1Router.use('/auth', authRouter);
+v1Router.use('/test-types', testTypeRouter);
 
 export default v1Router;

4400ff99b2e80bdd35de40546ff6308cc6e0d11f
Ameen Sid <ameensid7@outlook.com>
2026-05-25 19:53:45 +0530

    Added testing equipment router

---
+++ src/routers/v1/testing-equipment.router.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { TestingEquipmentFactory } from '../../factories/testing-equipment.factory';
+import { createTestingEquipmentSchema, updateTestingEquipmentSchema, testingEquipmentQuerySchema, testingEquipmentIdParamSchema } from '../../validators/testing-equipment.validator';
+import { validateRequestBody, validateQueryParams, validateRequestParams } from '../../validators';
+import { asyncHandler } from '../../utils/helpers/async.handler';
+
+const testingEquipmentRouter = express.Router();
+
+const testingEquipmentController = TestingEquipmentFactory.getTestingEquipmentController();
+
+testingEquipmentRouter.post(
+	'/',
+	validateRequestBody(createTestingEquipmentSchema),
+	asyncHandler(testingEquipmentController.addTestingEquipment)
+);
+
+testingEquipmentRouter.get(
+	'/',
+	validateQueryParams(testingEquipmentQuerySchema),
+	asyncHandler(testingEquipmentController.getTestingEquipments)
+);
+
+testingEquipmentRouter.patch(
+	'/:id',
+	validateRequestParams(testingEquipmentIdParamSchema),
+	validateRequestBody(updateTestingEquipmentSchema),
+	asyncHandler(testingEquipmentController.updateTestingEquipment)
+);
+
+testingEquipmentRouter.delete(
+	'/:id',
+	validateRequestParams(testingEquipmentIdParamSchema),
+	asyncHandler(testingEquipmentController.deleteTestingEquipment)
+);
+
+export default testingEquipmentRouter;

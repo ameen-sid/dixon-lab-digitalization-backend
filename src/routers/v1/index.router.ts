f7f67a85f0d6b0f38f46d2929b1fea976849e09f
Ameen Sid <ameensid7@outlook.com>
2026-05-25 16:07:42 +0530

    Added test protocol router in the v1 router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -5,6 +5,7 @@ import userRouter from './user.router';
 import authRouter from './auth.router';
 import testTypeRouter from './test-type.router';
 import testCategoryRouter from './test-category.router';
+import testProtocolRouter from './test-protocol.router';
 import { authenticateToken, requireRole } from '../../middlewares/auth.middleware';
 
 const v1Router = express.Router();
@@ -15,5 +16,6 @@ v1Router.use('/users', authenticateToken, requireRole(['Admin']), userRouter);
 v1Router.use('/auth', authRouter);
 v1Router.use('/test-types', authenticateToken, requireRole(['Admin']), testTypeRouter);
 v1Router.use('/test-categories', authenticateToken, requireRole(['Admin']), testCategoryRouter);
+v1Router.use('/test-protocols', authenticateToken, requireRole(['Admin']), testProtocolRouter);
 
 export default v1Router;

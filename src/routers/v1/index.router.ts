5e5b7cd5a13d44eaac112cbb2ea597672efda51d
Ameen Sid <ameensid7@outlook.com>
2026-05-25 15:09:23 +0530

    Added test category router in the v1 router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -4,6 +4,7 @@ import departmentRouter from './department.router';
 import userRouter from './user.router';
 import authRouter from './auth.router';
 import testTypeRouter from './test-type.router';
+import testCategoryRouter from './test-category.router';
 import { authenticateToken, requireRole } from '../../middlewares/auth.middleware';
 
 const v1Router = express.Router();
@@ -13,5 +14,6 @@ v1Router.use('/departments', authenticateToken, requireRole(['Admin']), departme
 v1Router.use('/users', authenticateToken, requireRole(['Admin']), userRouter);
 v1Router.use('/auth', authRouter);
 v1Router.use('/test-types', authenticateToken, requireRole(['Admin']), testTypeRouter);
+v1Router.use('/test-categories', authenticateToken, requireRole(['Admin']), testCategoryRouter);
 
 export default v1Router;

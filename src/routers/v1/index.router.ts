f552579ff74d2d470ff11eaf716ce9e51fcd33b2
Ameen Sid <ameensid7@outlook.com>
2026-06-02 12:50:32 +0530

    Added reliability checksheet router in the v1 router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -11,6 +11,7 @@ import supplierCustomerRouter from './supplier-customer.router';
 import testingEquipmentRouter from './testing-equipment.router';
 import testRequestRouter from './test-request.router';
 import platformAvailabilityRouter from './platform-availability.router';
+import reliabilityChecksheetRouter from './reliability-checksheet.router';
 import { authenticateToken, requireRole } from '../../middlewares/auth.middleware';
 
 const v1Router = express.Router();
@@ -19,13 +20,14 @@ v1Router.use('/ping', pingRouter);
 v1Router.use('/departments', authenticateToken, requireRole(['Admin']), departmentRouter);
 v1Router.use('/users', authenticateToken, requireRole(['Admin', 'Lab Manager', 'Head']), userRouter);
 v1Router.use('/auth', authRouter);
-v1Router.use('/test-types', authenticateToken, requireRole(['Admin', 'Lab Manager']), testTypeRouter);
-v1Router.use('/test-categories', authenticateToken, requireRole(['Admin', 'Lab Manager']), testCategoryRouter);
-v1Router.use('/test-protocols', authenticateToken, requireRole(['Admin', 'Lab Manager']), testProtocolRouter);
+v1Router.use('/test-types', authenticateToken, requireRole(['Admin', 'Lab Manager', 'Head', 'Engineer', 'Inspector']), testTypeRouter);
+v1Router.use('/test-categories', authenticateToken, requireRole(['Admin', 'Lab Manager', 'Head', 'Engineer', 'Inspector']), testCategoryRouter);
+v1Router.use('/test-protocols', authenticateToken, requireRole(['Admin', 'Lab Manager', 'Head', 'Engineer', 'Inspector']), testProtocolRouter);
 v1Router.use('/product-parts', authenticateToken, requireRole(['Admin']), productPartRouter);
 v1Router.use('/supplier-customers', authenticateToken, requireRole(['Admin']), supplierCustomerRouter);
-v1Router.use('/testing-equipments', authenticateToken, requireRole(['Admin', 'Lab Manager']), testingEquipmentRouter);
+v1Router.use('/testing-equipments', authenticateToken, requireRole(['Admin', 'Lab Manager', 'Head', 'Engineer', 'Inspector']), testingEquipmentRouter);
 v1Router.use('/test-requests', authenticateToken, testRequestRouter);
 v1Router.use('/platform-availability', authenticateToken, requireRole(['Admin', 'Lab Manager']), platformAvailabilityRouter);
+v1Router.use('/reliability-checksheets', authenticateToken, requireRole(['Admin', 'Lab Manager', 'Inspector']), reliabilityChecksheetRouter);
 
 export default v1Router;
